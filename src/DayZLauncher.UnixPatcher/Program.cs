301a019cd10ec1c82936639a8a1fb9402fde23ed
valters-tomsons <your@email.com>
2023-04-21 00:55:34 +0300

    Improve styling, cleanup patcher

--- src/DayZLauncher.UnixPatcher/Program.cs
+++ src/DayZLauncher.UnixPatcher/Program.cs
@@ -1,59 +1,68 @@
 ﻿using Mono.Cecil;
 using Mono.Cecil.Cil;
 
-if (args.Length != 1 || !Directory.Exists(args[0]))
+var baseDirectory = Path.GetDirectoryName(AppContext.BaseDirectory + '/');
+
+var userSelection = args[0].Trim();
+if (string.IsNullOrWhiteSpace(userSelection))
 {
     Console.WriteLine("Must provide path to DayZ installation folder as argument!");
-    Console.ReadKey();
     return;
 }
 
-var targetAssembly = $"{args?.FirstOrDefault()?.Trim()}/Launcher/Utils.dll";
+var targetAssembly = $"{userSelection}/Launcher/Utils.dll";
 if (!File.Exists(targetAssembly))
 {
-    Console.WriteLine(@"Could not find 'Launcher\Utils.dll' in target folder!");
-    Console.ReadKey();
+    Console.WriteLine("Could not find 'Launcher/Utils.dll' in target folder!");
     return;
 }
 
-var backupAssembly = targetAssembly + ".bak";
-if (File.Exists(backupAssembly))
-{
-    File.Delete(backupAssembly);
-}
+var backupAssembly = MoveFileToBackup(targetAssembly);
 
-File.Move(targetAssembly, backupAssembly);
+var utilsPatchPath = baseDirectory + "/DayZLauncher.UnixPatcher.Utils.dll";
+using var utilsPatchDef = AssemblyDefinition.ReadAssembly(utilsPatchPath);
+var unixJunctionsTypeDef = utilsPatchDef.MainModule.GetType("DayZLauncher.UnixPatcher.Utils.UnixJunctions");
 
-var currentDir = Path.GetDirectoryName(AppContext.BaseDirectory + '/');
-var patchAssembly = currentDir + "/DayZLauncher.UnixPatcher.Utils.dll";
+using var targetDef = AssemblyDefinition.ReadAssembly(backupAssembly);
+var unixJunctionsImport = targetDef.MainModule.ImportReference(unixJunctionsTypeDef);
 
-using var patchDefinition = AssemblyDefinition.ReadAssembly(patchAssembly);
-var unixJunctionsType = patchDefinition.MainModule.GetType("DayZLauncher.UnixPatcher.Utils.UnixJunctions");
+var targetJunctionsTypeDef = targetDef.MainModule.GetType("Utils.IO.Junctions");
 
-using var targetDefinition = AssemblyDefinition.ReadAssembly(backupAssembly);
-var importedUnixJunctionsType = targetDefinition.MainModule.ImportReference(unixJunctionsType);
-
-var junctionsClass = targetDefinition.MainModule.GetType("Utils.IO.Junctions");
-
-PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "Create", new List<OpCode> { OpCodes.Ldarg_0, OpCodes.Ldarg_1, OpCodes.Ldarg_2 });
-PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "Delete", new List<OpCode> { OpCodes.Ldarg_0 });
-PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "Exists", new List<OpCode> { OpCodes.Ldarg_0 });
-PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "GetTarget", new List<OpCode> { OpCodes.Ldarg_0 });
+PatchJunctionsMethod(unixJunctionsTypeDef, targetDef, targetJunctionsTypeDef, "Create", new List<OpCode> { OpCodes.Ldarg_0, OpCodes.Ldarg_1, OpCodes.Ldarg_2 });
+PatchJunctionsMethod(unixJunctionsTypeDef, targetDef, targetJunctionsTypeDef, "Delete", new List<OpCode> { OpCodes.Ldarg_0 });
+PatchJunctionsMethod(unixJunctionsTypeDef, targetDef, targetJunctionsTypeDef, "Exists", new List<OpCode> { OpCodes.Ldarg_0 });
+PatchJunctionsMethod(unixJunctionsTypeDef, targetDef, targetJunctionsTypeDef, "GetTarget", new List<OpCode> { OpCodes.Ldarg_0 });
 
 Console.WriteLine("Writing patches to disk...");
 
-targetDefinition.Write(targetAssembly);
-File.Copy(patchAssembly, args[0].Trim() + "/Launcher/DayZLauncher.UnixPatcher.Utils.dll", true);
+try
+{
+    File.Copy(utilsPatchPath, args[0].Trim() + "/Launcher/DayZLauncher.UnixPatcher.Utils.dll", true);
+    targetDef.Write(targetAssembly);
+}
+catch
+{
+    Console.WriteLine("Failed to write files into DayZ directory!");
+    Console.WriteLine("Aborting...");
+    File.Move(backupAssembly, targetAssembly);
+}
 
 Console.WriteLine("Patch applied!");
-Console.ReadKey();
 
 void PatchJunctionsMethod(TypeDefinition unixJunctionsType, AssemblyDefinition targetDefinition, TypeDefinition junctionsClass, string methodName, List<OpCode> args)
 {
     var originalMethod = junctionsClass.Methods.FirstOrDefault(m => m.Name == methodName);
     var patchedMethod = unixJunctionsType.Methods.FirstOrDefault(m => m.Name == methodName);
     var importedPatchedMethod = targetDefinition.MainModule.ImportReference(patchedMethod);
+
+    if (originalMethod is null)
+    {
+        Console.WriteLine("Failed to patch");
+        return;
+    }
+
     originalMethod.Body = new MethodBody(originalMethod);
+
     var il = originalMethod.Body.GetILProcessor();
 
     il.Emit(OpCodes.Nop);
@@ -65,4 +74,11 @@ void PatchJunctionsMethod(TypeDefinition unixJunctionsType, AssemblyDefinition t
 
     il.Emit(OpCodes.Call, importedPatchedMethod);
     il.Emit(OpCodes.Ret);
+}
+
+string MoveFileToBackup(string filePath)
+{
+    var newPath = filePath + ".bak";
+    File.Move(filePath, newPath, true);
+    return newPath;
 }
