3bb7ca12fd58085fc01c007facb0dc3feb62d1d8
Valters <valters@tomsons.me>
2023-04-20 21:59:02 +0200

    Convert to SDK style .csproj and use the new language features (#1)
    * Convert Utils csproj to sdk-style
    
    * Convert patcher csproj to sdk-style
    
    * Set output type to Exe
    
    * Rebuild solution with modern dotnet sdk
    
    * Convert to file-scoped namespaces
    
    * Use backupAssembly variable
    
    * Use new language features
    
    * Set LangVersion to 11
    
    * Import utils project
    
    ---------
    
    Co-authored-by: valters-tomsons <your@email.com>

--- DayZLauncher.UnixPatcher.Utils/UnixJunctions.cs
+++ DayZLauncher.UnixPatcher.Utils/UnixJunctions.cs
@@ -4,183 +4,184 @@ using System.IO;
 using System.Reflection;
 using System.Threading;
 
-namespace DayZLauncher.UnixPatcher.Utils
+namespace DayZLauncher.UnixPatcher.Utils;
+
+// Ignore warnings about unused method arguments
+#pragma warning disable RCS1163, IDE0060
+
+public static class UnixJunctions
 {
-    public class UnixJunctions
+    private static bool IsRunningOnMono => Type.GetType("Mono.Runtime") != null;
+
+    static UnixJunctions()
     {
-        private static bool IsRunningOnMono => Type.GetType("Mono.Runtime") != null;
-
-        static UnixJunctions()
+        if (IsRunningOnMono)
         {
-            if (IsRunningOnMono)
-            {
-                Console.WriteLine("UnixJunctions: running on Mono runtime!");
-            }
+            Console.WriteLine("UnixJunctions: running on Mono runtime!");
         }
-        
-        public static void Create(string junctionPoint, string targetDir, bool overwrite)
+    }
+
+    public static void Create(string junctionPoint, string targetDir, bool overwrite)
+    {
+        targetDir = Path.GetFullPath(targetDir);
+
+        if (Directory.Exists(junctionPoint))
         {
-            targetDir = Path.GetFullPath(targetDir);
-
-            if (Directory.Exists(junctionPoint))
-            {
-                Delete(junctionPoint);
-            }
-
-            junctionPoint = ToUnixPath(junctionPoint);
-            targetDir = ToUnixPath(targetDir);
-
-            RunShellCommand("ln", $"-s -T \"{targetDir}\" \"{junctionPoint}\"");
+            Delete(junctionPoint);
         }
 
-        public static void Delete(string junctionPoint)
-        {
-            if (!Directory.Exists(junctionPoint))
-            {
-                if (File.Exists(junctionPoint))
-                {
-                    throw new IOException("UnixJunctions: Path is not a junction point");
-                }
-                return;
-            }
+        junctionPoint = ToUnixPath(junctionPoint);
+        targetDir = ToUnixPath(targetDir);
 
-            if (Directory.Exists(junctionPoint))
+        RunShellCommand("ln", $"-s -T \"{targetDir}\" \"{junctionPoint}\"");
+    }
+
+    public static void Delete(string junctionPoint)
+    {
+        if (!Directory.Exists(junctionPoint))
+        {
+            if (File.Exists(junctionPoint))
             {
-                junctionPoint = ToUnixPath(junctionPoint);
-                RunShellCommand("rm", $"-r \"{junctionPoint}\"");
+                throw new IOException("UnixJunctions: Path is not a junction point");
             }
+            return;
         }
 
-        public static bool Exists(string path)
-        {
-            if (!Directory.Exists(path))
-            {
-                return false;
-            }
-
-            try
-            {
-                path = ToUnixPath(path);
-                string output = RunShellCommand("ls", $"-la \"{path}\"");
-                return output.Contains("->");
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        public static string GetTarget(string junctionPoint)
+        if (Directory.Exists(junctionPoint))
         {
             junctionPoint = ToUnixPath(junctionPoint);
-            string output = RunShellCommand("readlink", $"\"{junctionPoint}\"");
-            return output.Trim();
+            RunShellCommand("rm", $"-r \"{junctionPoint}\"");
         }
+    }
 
-        private static string RunShellCommand(string command, string arguments)
+    public static bool Exists(string path)
+    {
+        if (!Directory.Exists(path))
         {
-            Console.WriteLine("UnixJunctions.RunShellCommand: command= " + command + " ;arguments= " + arguments);
-
-            var gameLocation = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var basePath = gameLocation + @$"\!Linux";
-            Directory.CreateDirectory(basePath);
-
-            string uniqueId = Guid.NewGuid().ToString("N");
-            string tempScriptPath = basePath + @$"\tmp_script_{uniqueId}.sh";
-            string tempOutputPath = basePath + @$"\tmp_output_{uniqueId}.txt";
-            string lockFilePath = basePath + @$"\{uniqueId}.lock";
-
-            Console.WriteLine("UnixJunctions.RunShellCommand: tempScriptPath= " + tempScriptPath + " ;tempOutputPath= " + tempOutputPath);
-
-            // Write the shell script
-            var script = @$"#!/bin/sh
-touch ""{ToUnixPath(lockFilePath)}""
-{command} {arguments} > ""{ToUnixPath(tempOutputPath)}""
-rm ""{ToUnixPath(lockFilePath)}""
-
-";
-
-            File.WriteAllText(tempScriptPath, script.Replace("\r\n", "\n"));
-
-            Console.WriteLine("UnixJunctions.RunShellCommand: shell script generated " + uniqueId);
-
-            // Set executable permission on the script
-            var chmodStartInfo = new ProcessStartInfo
-            {
-                FileName = "cmd.exe",
-                Arguments = $"/C start /unix /usr/bin/chmod +x \"{ToUnixPath(tempScriptPath)}\"",
-                RedirectStandardOutput = false,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            Console.WriteLine("UnixJunctions.RunShellCommand: about to chmod " + uniqueId + tempScriptPath);
-
-            using (Process chmodProcess = new Process { StartInfo = chmodStartInfo })
-            {
-                chmodProcess.Start();
-                chmodProcess.WaitForExit();
-
-                if (chmodProcess.ExitCode != 0)
-                {
-                    throw new Exception($"UnixJunctions.RunShellCommand: Error setting executable permission on '{tempScriptPath}'. Exit code: {chmodProcess.ExitCode}");
-                }
-                else
-                {
-                    Console.WriteLine("UnixJunctions.RunShellCommand: chmod exited: " + chmodProcess.ExitCode);
-                }
-            }
-
-            Console.WriteLine("UnixJunctions.RunShellCommand: chmod finished for " + uniqueId);
-
-            // Execute the shell script
-            var processStartInfo = new ProcessStartInfo
-            {
-                FileName = "cmd.exe",
-                Arguments = $"/C start /unix /usr/bin/sh -c \"{ToUnixPath(tempScriptPath)}\"",
-                RedirectStandardOutput = false,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            Console.WriteLine("UnixJunctions.RunShellCommand: about to launch script " + uniqueId);
-
-            using (Process process = new Process { StartInfo = processStartInfo })
-            {
-                process.Start();
-                process.WaitForExit();
-
-                if (process.ExitCode != 0)
-                {
-                    throw new Exception($"UnixJunctions: Error running shell script '{tempScriptPath}'. Exit code: {process.ExitCode}");
-                }
-            }
-
-            while (!File.Exists(tempOutputPath))
-            {
-                Console.WriteLine("UnixJunctions.RunShellCommand: waiting for output file " + uniqueId);
-                Thread.Sleep(50);
-            }
-
-            while (File.Exists(lockFilePath))
-            {
-                Console.WriteLine("UnixJunctions.RunShellCommand: waiting for unix write unlock " + uniqueId);
-                Thread.Sleep(50);
-            }
-
-            // Read the output file
-            string output = File.ReadAllText(tempOutputPath);
-            Console.WriteLine("UnixJunctions.RunShellCommand: output= " + output);
-
-            File.Delete(tempOutputPath);
-            File.Delete(tempScriptPath);
-
-            return output;
+            return false;
         }
 
-        private static string ToUnixPath(string windowsPath)
+        try
         {
-            return windowsPath.Replace("Z:", string.Empty).Replace("\\", "/");
+            path = ToUnixPath(path);
+            string output = RunShellCommand("ls", $"-la \"{path}\"");
+            return output.Contains("->");
         }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public static string GetTarget(string junctionPoint)
+    {
+        junctionPoint = ToUnixPath(junctionPoint);
+        string output = RunShellCommand("readlink", $"\"{junctionPoint}\"");
+        return output.Trim();
+    }
+
+    private static string RunShellCommand(string command, string arguments)
+    {
+        Console.WriteLine("UnixJunctions.RunShellCommand: command= " + command + " ;arguments= " + arguments);
+
+        var gameLocation = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        var basePath = gameLocation + @"\!Linux";
+        Directory.CreateDirectory(basePath);
+
+        string uniqueId = Guid.NewGuid().ToString("N");
+        string tempScriptPath = basePath + @$"\tmp_script_{uniqueId}.sh";
+        string tempOutputPath = basePath + @$"\tmp_output_{uniqueId}.txt";
+        string lockFilePath = basePath + @$"\{uniqueId}.lock";
+
+        Console.WriteLine("UnixJunctions.RunShellCommand: tempScriptPath= " + tempScriptPath + " ;tempOutputPath= " + tempOutputPath);
+
+        var script = $"""
+        #!/bin/sh
+        touch "{ToUnixPath(lockFilePath)}"
+        {command} {arguments} > "{ToUnixPath(tempOutputPath)}"
+        rm "{ToUnixPath(lockFilePath)}"
+        """;
+
+        File.WriteAllText(tempScriptPath, script.Replace("\r\n", "\n"));
+
+        Console.WriteLine("UnixJunctions.RunShellCommand: shell script generated " + uniqueId);
+
+        // Set executable permission on the script
+        var chmodStartInfo = new ProcessStartInfo
+        {
+            FileName = "cmd.exe",
+            Arguments = $"/C start /unix /usr/bin/chmod +x \"{ToUnixPath(tempScriptPath)}\"",
+            RedirectStandardOutput = false,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        Console.WriteLine("UnixJunctions.RunShellCommand: about to chmod " + uniqueId + tempScriptPath);
+
+        using (Process chmodProcess = new() { StartInfo = chmodStartInfo })
+        {
+            chmodProcess.Start();
+            chmodProcess.WaitForExit();
+
+            if (chmodProcess.ExitCode != 0)
+            {
+                throw new Exception($"UnixJunctions.RunShellCommand: Error setting executable permission on '{tempScriptPath}'. Exit code: {chmodProcess.ExitCode}");
+            }
+            else
+            {
+                Console.WriteLine("UnixJunctions.RunShellCommand: chmod exited: " + chmodProcess.ExitCode);
+            }
+        }
+
+        Console.WriteLine("UnixJunctions.RunShellCommand: chmod finished for " + uniqueId);
+
+        // Execute the shell script
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "cmd.exe",
+            Arguments = $"/C start /unix /usr/bin/sh -c \"{ToUnixPath(tempScriptPath)}\"",
+            RedirectStandardOutput = false,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        Console.WriteLine("UnixJunctions.RunShellCommand: about to launch script " + uniqueId);
+
+        using (Process process = new() { StartInfo = processStartInfo })
+        {
+            process.Start();
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                throw new Exception($"UnixJunctions: Error running shell script '{tempScriptPath}'. Exit code: {process.ExitCode}");
+            }
+        }
+
+        while (!File.Exists(tempOutputPath))
+        {
+            Console.WriteLine("UnixJunctions.RunShellCommand: waiting for output file " + uniqueId);
+            Thread.Sleep(50);
+        }
+
+        while (File.Exists(lockFilePath))
+        {
+            Console.WriteLine("UnixJunctions.RunShellCommand: waiting for unix write unlock " + uniqueId);
+            Thread.Sleep(50);
+        }
+
+        // Read the output file
+        string output = File.ReadAllText(tempOutputPath);
+        Console.WriteLine("UnixJunctions.RunShellCommand: output= " + output);
+
+        File.Delete(tempOutputPath);
+        File.Delete(tempScriptPath);
+
+        return output;
+    }
+
+    private static string ToUnixPath(string windowsPath)
+    {
+        return windowsPath.Replace("Z:", string.Empty).Replace("\\", "/");
     }
 }
--- DayZLauncher.UnixPatcher/Program.cs
+++ DayZLauncher.UnixPatcher/Program.cs
@@ -5,77 +5,75 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace DayZLauncher.UnixPatcher
+namespace DayZLauncher.UnixPatcher;
+
+internal static class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        if (args.Length != 1 || !Directory.Exists(args[0]))
         {
-            if (args.Length != 1 || !Directory.Exists(args[0]))
-            {
-                Console.WriteLine("Must provide path to DayZ installation folder as argument!");
-                Console.ReadKey();
-                return;
-            }
-
-            var targetAssembly = @$"{args[0].Trim()}\Launcher\Utils.dll";
-            if (!File.Exists(targetAssembly))
-            {
-                Console.WriteLine(@"Could not find 'Launcher\Utils.dll' in target folder!");
-                Console.ReadKey();
-                return;
-            }
-
-            var backupAssembly = targetAssembly + ".bak";
-
-            if (File.Exists(backupAssembly))
-            {
-                File.Delete(backupAssembly);
-            }
-
-            File.Move(targetAssembly, targetAssembly + ".bak");
-
-            var patchAssembly = "DayZLauncher.UnixPatcher.Utils.dll";
-
-            using var patchDefinition = AssemblyDefinition.ReadAssembly(patchAssembly);
-            var unixJunctionsType = patchDefinition.MainModule.GetType("DayZLauncher.UnixPatcher.Utils.UnixJunctions");
-
-            using var targetDefinition = AssemblyDefinition.ReadAssembly(targetAssembly + ".bak");
-            var importedUnixJunctionsType = targetDefinition.MainModule.ImportReference(unixJunctionsType);
-
-            var junctionsClass = targetDefinition.MainModule.GetType("Utils.IO.Junctions");
-
-            PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "Create", new List<OpCode> { OpCodes.Ldarg_0, OpCodes.Ldarg_1, OpCodes.Ldarg_2 });
-            PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "Delete", new List<OpCode> { OpCodes.Ldarg_0 });
-            PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "Exists", new List<OpCode> { OpCodes.Ldarg_0 });
-            PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "GetTarget", new List<OpCode> { OpCodes.Ldarg_0 });
-
-            Console.WriteLine("Writing patches to disk...");
-
-            targetDefinition.Write(targetAssembly);
-            File.Copy("DayZLauncher.UnixPatcher.Utils.dll", @$"{args[0].Trim()}\Launcher\DayZLauncher.UnixPatcher.Utils.dll", true);
-
-            Console.WriteLine("Patch applied!");
+            Console.WriteLine("Must provide path to DayZ installation folder as argument!");
             Console.ReadKey();
+            return;
         }
 
-        private static void PatchJunctionsMethod(TypeDefinition unixJunctionsType, AssemblyDefinition targetDefinition, TypeDefinition junctionsClass, string methodName, List<OpCode> args)
+        var targetAssembly = @$"{args[0].Trim()}\Launcher\Utils.dll";
+        if (!File.Exists(targetAssembly))
         {
-            var originalMethod = junctionsClass.Methods.FirstOrDefault(m => m.Name == methodName);
-            var patchedMethod = unixJunctionsType.Methods.FirstOrDefault(m => m.Name == methodName);
-            var importedPatchedMethod = targetDefinition.MainModule.ImportReference(patchedMethod);
-            originalMethod.Body = new MethodBody(originalMethod);
-            var il = originalMethod.Body.GetILProcessor();
-
-            il.Emit(OpCodes.Nop);
-
-            foreach(var op in args)
-            {
-                il.Emit(op);
-            }
-
-            il.Emit(OpCodes.Call, importedPatchedMethod);
-            il.Emit(OpCodes.Ret);
+            Console.WriteLine(@"Could not find 'Launcher\Utils.dll' in target folder!");
+            Console.ReadKey();
+            return;
         }
+
+        var backupAssembly = targetAssembly + ".bak";
+        if (File.Exists(backupAssembly))
+        {
+            File.Delete(backupAssembly);
+        }
+
+        File.Move(targetAssembly, backupAssembly);
+
+        const string patchAssembly = "DayZLauncher.UnixPatcher.Utils.dll";
+
+        using var patchDefinition = AssemblyDefinition.ReadAssembly(patchAssembly);
+        var unixJunctionsType = patchDefinition.MainModule.GetType("DayZLauncher.UnixPatcher.Utils.UnixJunctions");
+
+        using var targetDefinition = AssemblyDefinition.ReadAssembly(backupAssembly);
+        var importedUnixJunctionsType = targetDefinition.MainModule.ImportReference(unixJunctionsType);
+
+        var junctionsClass = targetDefinition.MainModule.GetType("Utils.IO.Junctions");
+
+        PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "Create", new List<OpCode> { OpCodes.Ldarg_0, OpCodes.Ldarg_1, OpCodes.Ldarg_2 });
+        PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "Delete", new List<OpCode> { OpCodes.Ldarg_0 });
+        PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "Exists", new List<OpCode> { OpCodes.Ldarg_0 });
+        PatchJunctionsMethod(unixJunctionsType, targetDefinition, junctionsClass, "GetTarget", new List<OpCode> { OpCodes.Ldarg_0 });
+
+        Console.WriteLine("Writing patches to disk...");
+
+        targetDefinition.Write(targetAssembly);
+        File.Copy("DayZLauncher.UnixPatcher.Utils.dll", @$"{args[0].Trim()}\Launcher\DayZLauncher.UnixPatcher.Utils.dll", true);
+
+        Console.WriteLine("Patch applied!");
+        Console.ReadKey();
+    }
+
+    private static void PatchJunctionsMethod(TypeDefinition unixJunctionsType, AssemblyDefinition targetDefinition, TypeDefinition junctionsClass, string methodName, List<OpCode> args)
+    {
+        var originalMethod = junctionsClass.Methods.FirstOrDefault(m => m.Name == methodName);
+        var patchedMethod = unixJunctionsType.Methods.FirstOrDefault(m => m.Name == methodName);
+        var importedPatchedMethod = targetDefinition.MainModule.ImportReference(patchedMethod);
+        originalMethod.Body = new MethodBody(originalMethod);
+        var il = originalMethod.Body.GetILProcessor();
+
+        il.Emit(OpCodes.Nop);
+
+        foreach (var op in args)
+        {
+            il.Emit(op);
+        }
+
+        il.Emit(OpCodes.Call, importedPatchedMethod);
+        il.Emit(OpCodes.Ret);
     }
 }
